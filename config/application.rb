64f61c0cb33aff741d762fee0c27f9e8adb23c38
Nick Alzapiedi <alzapiedi@gmail.com>
2016-01-27 19:38:31 -0500

    Added images to seeds

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,7 @@ module SampleProjectProposal
       :storage => :s3,
       :bucket => ENV["s3_bucket"],
       :s3_credentials => {
+        :bucket => ENV["s3_bucket"],
         :access_key_id => ENV["s3_access_key_id"],
         :secret_access_key => ENV["s3_secret_access_key"]
       }
